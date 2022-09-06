1bdbc0f965348f3bfdf4cd95fdde6afb11121d44
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-09-06 20:29:06 -0300

    Update 0.2

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Capítulo 1",
+        body: "Lorem ipsum",
+      },
+      {
+        id: 2,
+        title: "Capítulo 2",
+        body: "Lorem ipsum",
+      },
+      {
+        id: 3,
+        title: "Capítulo 3",
+        body: "Lorem ipsum",
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map((posts) => (
+          <div key={posts.id}>
+            <h1>{posts.title}</h1>
+            <p>{posts.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
