8783be49fd16442da19da4f085534a97baf4e9f0
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-09-07 12:39:39 -0300

    Update 0.3
    - Input search;
    - Button nexPage.

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -7,24 +7,66 @@ import { loadPost } from "./utils/loadPosts";
 class App extends Component {
   state = {
     posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 50,
+    searchValue: "",
   };
 
   async componentDidMount() {
     await this.loadPost();
+
+    const {posts, allPosts, page, postsPerPage} = this.state;
+    this.setState({posts: allPosts.slice(page, postsPerPage)});
+  }
+
+  postSearch = (e) => {
+    const { value } = e.target;
+
+    const {posts, allPosts, searchValue} = this.state;
+
+    this.setState({searchValue: value});
+  }
+
+  methodButton = () => {
+    const {page, postsPerPage, allPosts, posts} = this.state;
+
+    const pageSum = page + postsPerPage;
+    const nextPage = pageSum + postsPerPage;
+    const nextPosts = allPosts.slice(pageSum, nextPage);
+    posts.push(...nextPosts)
+
+    this.setState({ posts, page : nextPage});
   }
   
   loadPost = async () => {
     const postsAndPhotos = await loadPost();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({ allPosts: postsAndPhotos });
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, allPosts, searchValue } = this.state;
+
+    var bool = false;
+
+    if (posts.length === allPosts.length) {
+      bool = true;
+    }
+
+    const filterPost = !!searchValue ?
+      posts.filter((item) => {
+      return item.title.toLowerCase().includes(searchValue.toLowerCase());
+    }):posts
 
     return (
       <section className="container">
+        <input 
+        type="search" 
+        onChange={this.postSearch}
+        /> <br /> <br /> <br />
         <Post 
-        posts={posts} />
+        posts={filterPost} />
+        <button onClick={this.methodButton} disabled={bool}>Load more pages</button>
       </section>
     );
   }
